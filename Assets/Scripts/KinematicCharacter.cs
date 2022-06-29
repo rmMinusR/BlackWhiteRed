1291b3d402c2597b101c30a563ac59f615cff309
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-06-29 17:20:20 -0400

    Player movement, somewhat tuned

---
+++ Assets/Scripts/KinematicCharacter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public sealed class KinematicCharacter : MonoBehaviour
+{
+    public CharacterController coll { get; private set; }
+
+    private void Start()
+    {
+        coll = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Step(Time.fixedTime, Time.fixedDeltaTime);
+    }
+
+    //Transient I/O
+    [NonSerialized] public Vector3 velocity;
+    public CollisionFlags contactAreas { get; private set; }
+
+    //Tweakable in inspector, but mutable at runtime
+    public bool enableGravity = true;
+    public float gravityScale = 1;
+    public Vector3 GravityExperienced => enableGravity ? Physics.gravity*gravityScale : Vector3.zero;
+
+    //Fixed settings
+    [SerializeField] private bool suspendGravityOnGround = true;
+
+    private void Step(float t, float dt)
+    {
+        //May cause funky behaviour going down slopes. TODO test
+        if (!suspendGravityOnGround || !coll.isGrounded) velocity += GravityExperienced * dt;
+
+        contactAreas = coll.Move(velocity * dt);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        //Prevent building speed running into walls
+        velocity = Vector3.ProjectOnPlane(velocity, hit.normal);
+    }
+}
---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    [Header("Movement settings")]
+    [SerializeField] [Range(0, 1)] private float groundSlipperiness = 0.1f;
+    [SerializeField] [Range(0, 1)] private float airSlipperiness = 0.1f;
+    [SerializeField] [Min(0)] private float speed = 5f;
+    [SerializeField] [Min(0)] private float jumpPower = 5f;
+
+    [Header("Network")]
+    [SerializeField] private ulong playerID;
+
+    [Header("Bindings")]
+    [SerializeField] private KinematicCharacter kinematicsLayer;
+    [SerializeField] private Transform frameOfReference;
+    [SerializeField] private PlayerInput playerInput;
+    private InputAction controlMove;
+    [SerializeField] private string controlMoveName = "Move";
+    private InputAction controlJump;
+    [SerializeField] private string controlJumpName = "Jump";
+
+    private void OnEnable()
+    {
+        controlMove = playerInput.currentActionMap.FindAction(controlMoveName);
+        controlJump = playerInput.currentActionMap.FindAction(controlJumpName);
+    }
+
+    private Vector2 rawInput;
+    private bool jumpPressed;
+
+    private void Update()
+    {
+        rawInput = controlMove.ReadValue<Vector2>();
+        jumpPressed = controlJump.IsPressed();
+    }
+
+    private void FixedUpdate()
+    {
+        //Handle horizontal movement
+        Vector3 targetVelocity = speed * (
+                frameOfReference.right   * rawInput.x
+              + frameOfReference.forward * rawInput.y
+            );
+
+        float slippageThisFrame = Mathf.Pow(kinematicsLayer.coll.isGrounded ? groundSlipperiness : airSlipperiness, Time.fixedDeltaTime);
+        Vector3 newVel = Vector3.Lerp(targetVelocity, kinematicsLayer.velocity, slippageThisFrame);
+        kinematicsLayer.velocity = new Vector3(newVel.x, kinematicsLayer.velocity.y, newVel.z);
+
+        //Handle jumping
+        if (jumpPressed && kinematicsLayer.coll.isGrounded)
+        {
+            kinematicsLayer.velocity += jumpPower * transform.up;
+        }
+    }
+}
