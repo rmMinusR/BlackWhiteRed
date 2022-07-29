b702dd18f4e0ffeb8410f15aa1eadfe202f812bd
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-07-29 14:19:25 -0400

    Notification replication

--- Assets/Scripts/UI/Notification Feed/NotificationFeed.cs
+++ Assets/Scripts/UI/Notification Feed/NotificationFeed.cs
@@ -5,12 +5,13 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-public sealed class NotificationFeed : MonoBehaviour
+public sealed class NotificationFeed : NetworkBehaviour
 {
-    [TestButton("Test kill message", nameof(__TestKillMessage), isActiveAtRuntime = true, isActiveInEditor = false, order = 100)]
-    [TestButton("Spam kill message", nameof(__SpamKillMessage), isActiveAtRuntime = true, isActiveInEditor = false, order = 101)]
-    [TestButton("Test close"       , nameof(__TestClose      ), isActiveAtRuntime = true, isActiveInEditor = false, order = 200)]
-    [TestButton("Test close all"   , nameof(__TestClear      ), isActiveAtRuntime = true, isActiveInEditor = false, order = 202)]
+    [TestButton("Test kill message"  , nameof(__TestKillMessage  ), isActiveAtRuntime = true, isActiveInEditor = false, order = 100)]
+    [TestButton("Spam kill message"  , nameof(__SpamKillMessage  ), isActiveAtRuntime = true, isActiveInEditor = false, order = 101)]
+    [TestButton("Test scored message", nameof(__TestScoredMessage), isActiveAtRuntime = true, isActiveInEditor = false, order = 120)]
+    [TestButton("Test close"         , nameof(__TestClose        ), isActiveAtRuntime = true, isActiveInEditor = false, order = 200)]
+    [TestButton("Test close all"     , nameof(__TestClear        ), isActiveAtRuntime = true, isActiveInEditor = false, order = 202)]
     [SerializeField] private Notification entryPrefab;
 
     [Space]
@@ -42,31 +43,96 @@ public sealed class NotificationFeed : MonoBehaviour
     [Header("TESTING")]
     [SerializeField] private PlayerController testKiller;
     [SerializeField] private PlayerController testKilled;
-    private void __TestKillMessage()
-    {
-        Add()
-            .Background(false)
-            .WithPlayer(testKiller)
-            .WithIcon(null)
-            .WithPlayer(testKilled)
-            .PrettyOpen();
-    }
+    [SerializeField] private KillSource testKillSource;
+    private void __TestKillMessage() => ShowDeathMessage(testKiller, testKilled, testKillSource);
+    private void __SpamKillMessage() { for (int i = 0; i < 5; ++i) __TestKillMessage(); }
+    private void __TestScoredMessage() => ShowScoredMessage(testKiller);
 
-    private void __SpamKillMessage()
-    {
-        for (int i = 0; i < 5; ++i) __TestKillMessage();
-    }
-
-    private void __TestClose()
-    {
-        entries[0].PrettyClose();
-    }
-
+    private void __TestClose() => entries[0].PrettyClose();
     private void __TestClear() => Clear(true);
 
     #endregion
 
-    public Notification Add()
+    #region Notification type definitions
+
+    [Header("Icons")]
+    [SerializeField] private Sprite swordKillIcon;
+    [SerializeField] private Sprite   bowKillIcon;
+    [SerializeField] private Sprite   fellOffIcon;
+    [SerializeField] private Sprite    scoredIcon;
+
+    [Flags]
+    public enum KillSource
+    {
+        Sword = (1 << 0),
+        Bow   = (1 << 1),
+
+        FellOff = (1 << 8)
+    }
+
+    //Helpers
+    private void EnsureHasSpawnPermission()
+    {
+        if (NetworkManager == null || !(NetworkManager.IsServer || NetworkManager.IsClient)) throw new InvalidOperationException("Not connected!");
+        if (!IsServer) throw new AccessViolationException("Only server may send notifications!");
+    }
+
+    private const ulong NULL_PLAYER_ID = ulong.MaxValue;
+    private static ulong TxPlayer(PlayerController c, bool require = true) => require||c!=null ? c.OwnerClientId : NULL_PLAYER_ID;
+    private static PlayerController RxPlayer(ulong id, bool require = true) => require||id!=NULL_PLAYER_ID ? NetHeartbeat.Of(id).GetComponent<PlayerController>() : null;
+
+    #region Death message
+
+    public void ShowDeathMessage(PlayerController killer, PlayerController killed, KillSource killSource)
+    {
+        EnsureHasSpawnPermission();
+        DONOTCALL_ShowDeathMessage_ClientRpc(TxPlayer(killer, require: false), TxPlayer(killed), killSource); //Broadcast to all
+    }
+
+    [ClientRpc]
+    private void DONOTCALL_ShowDeathMessage_ClientRpc(ulong killerID, ulong killedID, KillSource killSource, ClientRpcParams p = default)
+    {
+        PlayerController killer = RxPlayer(killerID, require: false);
+        PlayerController killed = RxPlayer(killedID);
+
+        Notification notif = Add().Background(killerID == OwnerClientId);
+        if (killer != null) notif.WithPlayer(killer);
+
+        //Write relevant icons
+        if (killSource.HasFlag(KillSource.Sword  )) notif.WithIcon(swordKillIcon);
+        if (killSource.HasFlag(KillSource.Bow    )) notif.WithIcon(  bowKillIcon);
+        if (killSource.HasFlag(KillSource.FellOff)) notif.WithIcon(  fellOffIcon);
+
+        notif.WithPlayer(killed);
+    }
+
+    #endregion
+
+    #region Scored message
+
+    public void ShowScoredMessage(PlayerController whoScored)
+    {
+        EnsureHasSpawnPermission();
+        DONOTCALL_ShowScoredMessage_ClientRpc(TxPlayer(whoScored)); //Broadcast to all
+    }
+
+    [ClientRpc]
+    private void DONOTCALL_ShowScoredMessage_ClientRpc(ulong whoScoredID, ClientRpcParams p = default)
+    {
+        PlayerController whoScored = RxPlayer(whoScoredID);
+
+        Add()
+            .Background(whoScoredID == OwnerClientId)
+            .WithPlayer(whoScored)
+            .WithIcon(scoredIcon);
+    }
+
+    #endregion
+
+    #endregion
+
+
+    private Notification Add() //Would be public, but we need to make sure Notifications are only spawned through the Show commands
     {
         //OverflowCheck();
 
