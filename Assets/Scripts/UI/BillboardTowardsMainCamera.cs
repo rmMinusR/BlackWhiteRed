0252a177b287fc363cf4614d86e39ac9f2652451
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-08-02 14:33:55 -0400

    Added player name tags

---
+++ Assets/Scripts/UI/BillboardTowardsMainCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public sealed class BillboardTowardsMainCamera : MonoBehaviour
+{
+    private Transform target;
+
+    void Update()
+    {
+        if (target == null) target = Camera.main.transform;
+
+        if (target != null) transform.rotation = target.rotation;
+    }
+}
---
+++ Assets/Scripts/UI/LayerByAlly.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+public sealed class LayerByAlly : NetworkBehaviour
+{
+    [SerializeField] private PlayerController playerController;
+    [SerializeField] [Min(0)] private int ifAlly;
+    [SerializeField] [Min(0)] private int ifEnemy;
+
+    private void Update()
+    {
+        if(IsSpawned)
+        {
+            //TODO call only on team assignment (and on spawn)
+
+            int targetLayer = (NetHeartbeat.Self.GetComponent<PlayerController>().CurrentTeam == playerController.CurrentTeam)
+                ? ifAlly
+                : ifEnemy;
+
+            if (gameObject.layer != targetLayer) gameObject.layer = targetLayer;
+        }
+    }
+}
