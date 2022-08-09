723402a2a3e2cbcb633d4b11d0fb343508af0b77
Nicholas Perell <58918508+nicholas-hoy-champain@users.noreply.github.com>
2022-08-09 19:37:49 -0400

    Update OptionsPanel.cs

--- Assets/Scripts/UI/OptionsPanel.cs
+++ Assets/Scripts/UI/OptionsPanel.cs
@@ -2,17 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct PlayerPrefOptionsBasis
+{
+    public string preferenceKey;
+    public float defaultValue;
+
+    public PlayerPrefOptionsBasis(string _key, float _value)
+    {
+        preferenceKey = _key;
+        defaultValue = _value;
+    }
+}
+
 public class OptionsPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //Keys for Player Preferences
+    public static readonly PlayerPrefOptionsBasis MOUSE_PREF = new PlayerPrefOptionsBasis("MOUSE_SENSITIVITY", 1.0f);
+    public static readonly PlayerPrefOptionsBasis SCROLL_PREF = new PlayerPrefOptionsBasis("SCROLL_SENSITIVITY",60.0f);
+    public static readonly PlayerPrefOptionsBasis MUSIC_PREF = new PlayerPrefOptionsBasis("MUSIC_VOLUME", 1.0f);
+    public static readonly PlayerPrefOptionsBasis SFX_PREF = new PlayerPrefOptionsBasis("SFX_VOLUME",1.0f);
+
+    //Variables
+    float mouseValue;
+    float scrollValue;
+    float musicValue;
+    float sfxValue;
+
+    void OnEnable()
     {
-        
+        CheckForPrefs();
     }
 
-    // Update is called once per frame
-    void Update()
+    void CheckForPrefs()
     {
-        
+        //Mouse
+        CheckPref(MOUSE_PREF, ref mouseValue);
+        PlayerLookController.sensitivity = Vector2.one * mouseValue;
+
+        //Scroll
+        CheckPref(SCROLL_PREF, ref scrollValue);
+        PlayerWeaponHolding.scrollThreshold = mouseValue;
+
+        //TODO: Music
+        CheckPref(MUSIC_PREF, ref musicValue);
+
+        //TODO: SFX
+        CheckPref(SFX_PREF, ref sfxValue);
+    }
+
+    void CheckPref(PlayerPrefOptionsBasis basis, ref float value)
+    {
+        if (PlayerPrefs.HasKey(basis.preferenceKey))
+        {
+            value = PlayerPrefs.GetFloat(basis.preferenceKey);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(basis.preferenceKey, basis.defaultValue);
+        }
     }
 }
