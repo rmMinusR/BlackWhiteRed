26f3d1dfb400f6c97d47ef24a7a8858c114e342a
Robert Christensen <robert.christensen@mymail.champlain.edu>
2022-08-09 15:49:58 -0400

    Initial implementation of announcement banner

--- Assets/Scripts/UI/AnnouncementBannerDriver.cs
+++ Assets/Scripts/UI/AnnouncementBannerDriver.cs
@@ -1,23 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class AnnouncementBannerDriver : MonoBehaviour
 {
     //Public interface function
     public void Show(string text, float duration) => queue.Add(new Entry { text = text, duration = duration });
 
-    [SerializeField] private Text textTarget;
-    [SerializeField] private Animation animTarget;
-    [SerializeField] private AnimationClip animOpen;
-    [SerializeField] private AnimationClip animClose;
+    [SerializeField] private RectTransform contentRoot;
+    [SerializeField] private TMP_Text textTarget;
+    [SerializeField] private float padding;
+    [SerializeField] private AnimationCurve animOpen  = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    [SerializeField] private AnimationCurve animClose = AnimationCurve.EaseInOut(0, 1, 1, 0);
 
     [Space]
     [SerializeField] private List<Entry> queue;
     [SerializeField] [InspectorReadOnly] private State state = State.Closed;
     [SerializeField] [InspectorReadOnly] private float timeUntilClose = 0;
+    [SerializeField] [InspectorReadOnly] private float animationTime = 0;
 
     [Flags] public enum State
     {
@@ -29,41 +31,64 @@ public class AnnouncementBannerDriver : MonoBehaviour
         Opening = Open   | Animating,
     }
 
+    [Serializable]
     public struct Entry
     {
         [Min(0)] public float duration;
         public string text;
     }
 
-    void Awake()
+    private void Start()
     {
-        animTarget = GetComponent<Animation>();
+        //Start closed
+        contentRoot.sizeDelta = new Vector2(0, contentRoot.sizeDelta.y);
     }
 
     void Update()
     {
+        contentRoot.gameObject.SetActive(state != State.Closed);
+
+        //Tick animation
+        if (state.HasFlag(State.Animating))
+        {
+            AnimationCurve anim = state switch
+            {
+                State.Opening => animOpen,
+                State.Closing => animClose,
+                _ => throw new NotImplementedException(),
+            };
+            animationTime += Time.deltaTime;
+
+            //Write to transform
+            contentRoot.sizeDelta = new Vector2(
+                (textTarget.rectTransform.sizeDelta.x + 2*padding) * anim.Evaluate(animationTime),
+                contentRoot.sizeDelta.y
+            );
+
+            //Mark if done animating
+            if (animationTime >= anim.keys[anim.length-1].time) state &= ~State.Animating;
+        }
+
         //Close after a period of time being open
         if (state == State.Open)
         {
             timeUntilClose -= Time.deltaTime;
-            if (timeUntilClose <= 0) animTarget.Play(animClose.name);
+            if (timeUntilClose <= 0)
+            {
+                state = State.Closing;
+                animationTime = 0;
+            }
         }
 
-        //Mark if done animating
-        if (state.HasFlag(State.Animating) && !animTarget.isPlaying) state &= ~State.Animating;
-
-        //If closed, try to show next entry in queue
+        //If closed, try to open next entry in queue
         if (state == State.Closed && queue.Count > 0)
         {
             textTarget.text = queue[0].text;
             timeUntilClose = queue[0].duration;
-            animTarget.Play(animOpen.name);
+            queue.RemoveAt(0);
+
+            state = State.Opening;
+            animationTime = 0;
         }
     }
-
-    //Animation callbacks
-    public void OnBeganOpening() => state = State.Opening;
-    public void OnBeganClosing() => state = State.Closing;
-    public void OnFinishedOpening() => state = State.Open;
-    public void OnFinishedClosing() => state = State.Closed;
 }
